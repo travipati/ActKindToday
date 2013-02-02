0becd69c385a3278e40e0f6b9b4043abfabf6d32
Teja Ravipati <ravipati@umich.edu>
2013-02-02 16:14:18 -0500

    Updated point implementation
    points are now stored on android internal

--- src/karma/koders/akt/ActKindTodayActivity.java
+++ src/karma/koders/akt/ActKindTodayActivity.java
@@ -3,42 +3,55 @@ package karma.koders.akt;
 import java.util.Random;
 
 import android.app.Activity;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.TextView;
 
-public class ActKindTodayActivity extends Activity {
-
-    int points = 0;
+public class ActKindTodayActivity extends Activity implements OnClickListener {
+	
+	TextView rak;
+    Button upPoint;
+    Button downPoint;
+    public static String filename = "MyStoredData";
+    SharedPreferences data;
+    
+    String rakList[] = {"test0", "test1", "test2", "test3", "test4", "test5"};
 	
 	@Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+
+        setUpVariables();
         
         Random randomGenerator = new Random();
-        String rakList[] = {"test0", "test1", "test2", "test3", "test4", "test5"};
-        
-        TextView rak = (TextView) findViewById(R.id.tvRak);
-        Button upPoint = (Button) findViewById(R.id.bSuccess);
-        Button downPoint = (Button) findViewById(R.id.bFail);
-        
         int rIndex = randomGenerator.nextInt(6);
         rak.setText(rakList[rIndex]);
-        
-        upPoint.setOnClickListener(new View.OnClickListener() {
-			
-			public void onClick(View v) {
-				points++;
-			}
-		});
-        
-        downPoint.setOnClickListener(new View.OnClickListener() {
-			
-			public void onClick(View v) {
-			
-			}
-		});
     }
+	
+	private void setUpVariables() {
+        rak = (TextView) findViewById(R.id.tvRak);
+        upPoint = (Button) findViewById(R.id.bSuccess);
+        downPoint = (Button) findViewById(R.id.bFail);
+        upPoint.setOnClickListener(this);
+        downPoint.setOnClickListener(this);
+	}
+
+	public void onClick(View v) {
+		switch (v.getId()) {
+		case R.id.bSuccess:
+	        data = getSharedPreferences(filename, 0);
+			int points = data.getInt("points", -1);
+			points++;
+			SharedPreferences.Editor editor = data.edit();
+			editor.putInt("points", points);
+			editor.commit();
+			break;
+		case R.id.bFail:
+			break;
+		}
+	}
 }
